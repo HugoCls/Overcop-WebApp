4e73823bdb92daa1b1c00e5ed05af1039fd44543
Hugo Colson <hugo.colson@mc2i.fr>
2023-11-08 10:54:23 +0100

    Add tables

---
+++ pages/Tables.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import pandas as pd
+
+from sqlalchemy import create_engine
+
+engine = create_engine(f'mysql://{st.secrets["MYSQL_USERNAME"]}:{st.secrets["MYSQL_PASSWORD"]}@{st.secrets["VPS_IP"]}/overcop')
+
+df_logs = pd.read_sql("SELECT * FROM Priceslogs", con=engine)
+
+df_stock = pd.read_sql("SELECT * FROM Database", con=engine)
+
+st.markdown("# Tables :signal_strength:")
+st.sidebar.markdown("# Tables :signal_strength:")
+
+st.title("Priceslogs")
+st.dataframe(df_logs)
+
+st.title("Database")
+st.dataframe(df_stock)
