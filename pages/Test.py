eef04b9c9c64f3be271c200e48e120c0d06b3be4
HugoCls <hugocls.pro@gmail.com>
2023-11-24 20:23:25 +0100

    Update

--- pages/Test.py
+++ pages/Test.py
@@ -13,10 +13,7 @@ if not os.path.exists(log_dir):
 
 if not os.path.exists(log_file_path):
     with open(log_file_path, 'w') as file:
-        file.write("")  # Crée le fichier s'il n'existe pas
-    
-with open(log_file_path, 'a') as f:
-    f.write('This is a test')
+        file.write("")
 
 if st.button('Folders & Files'):
     st.text(f"{os.getcwd()}")
@@ -27,11 +24,8 @@ if st.button('Folders & Files'):
         for fichier in fichiers:
             st.text(os.path.join(dossier_parent, fichier))
 
-st.text(log_file_path)
 
-st.text(os.path.exists(log_file_path))
-
-log.basicConfig(filename=f'{os.getcwd()}/data/console.log', level=log.INFO, format='%(asctime)s [%(levelname)s] %(filename)s - %(message)s')
+log.basicConfig(filename=log_file_path, level=log.INFO, format='%(asctime)s [%(levelname)s] %(filename)s - %(message)s')
 
 if len(log.getLogger('').handlers) < 2:  # Vérifiez le nombre de handlers pour éviter les doublons
     temp_file_handler = log.FileHandler('data/temp_logs.log')
@@ -40,13 +34,9 @@ if len(log.getLogger('').handlers) < 2:  # Vérifiez le nombre de handlers pour 
     log.getLogger('').addHandler(temp_file_handler)
 
 if st.button('Log test'):
-    #print("TEST")
     log.info("TEST")
-    #print("HELLO")
 
-with open('data/console.log', 'r') as file:
+with open(log_file_path, 'r') as file:
     logs_content = file.read()
-
-log.info("THIS IS A LOG")
 
 st.text_area("Logs temporaires:", value=logs_content, height=min(600, logs_content.count('\n') + 1))
