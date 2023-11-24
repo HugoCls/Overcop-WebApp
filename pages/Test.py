097e79d8381fbc3190f1a0a083f51f2a0739b42e
HugoCls <hugocls.pro@gmail.com>
2023-11-24 20:12:13 +0100

    Update

--- pages/Test.py
+++ pages/Test.py
@@ -2,17 +2,15 @@ import logging as log
 import streamlit as st
 import os
 import sys
-"""
+
 if st.button('Folders & Files'):
     st.text(f"{os.getcwd()}")
 
     for dossier_parent, dossiers, fichiers in os.walk(os.getcwd()):
         for dossier in dossiers:
-            print(os.path.join(dossier_parent, dossier))
+            st.text(os.path.join(dossier_parent, dossier))
         for fichier in fichiers:
-            print(os.path.join(dossier_parent, fichier))
-
-"""
+            st.text(os.path.join(dossier_parent, fichier))
 
 st.text(f"{os.getcwd()}/data/console.log")
 
