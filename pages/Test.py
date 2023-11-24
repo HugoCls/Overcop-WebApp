6b7487fcea351886f52249af21a494e99bc27797
HugoCls <hugocls.pro@gmail.com>
2023-11-24 20:11:30 +0100

    Update

--- pages/Test.py
+++ pages/Test.py
@@ -16,7 +16,7 @@ if st.button('Folders & Files'):
 
 st.text(f"{os.getcwd()}/data/console.log")
 
-log.basicConfig(stream=sys.stdout, filename=f'{os.getcwd()}/data/console.log', level=log.INFO, format='%(asctime)s [%(levelname)s] %(filename)s - %(message)s')
+log.basicConfig(filename=f'{os.getcwd()}/data/console.log', level=log.INFO, format='%(asctime)s [%(levelname)s] %(filename)s - %(message)s')
 
 if len(log.getLogger('').handlers) < 2:  # Vérifiez le nombre de handlers pour éviter les doublons
     temp_file_handler = log.FileHandler('data/temp_logs.log')
