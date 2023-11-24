6da3e43c6defc2a7b5e3eeaf373cd072f8ccd4c2
HugoCls <hugocls.pro@gmail.com>
2023-11-24 20:10:42 +0100

    Update

--- pages/Test.py
+++ pages/Test.py
@@ -14,6 +14,8 @@ if st.button('Folders & Files'):
 
 """
 
+st.text(f"{os.getcwd()}/data/console.log")
+
 log.basicConfig(stream=sys.stdout, filename=f'{os.getcwd()}/data/console.log', level=log.INFO, format='%(asctime)s [%(levelname)s] %(filename)s - %(message)s')
 
 if len(log.getLogger('').handlers) < 2:  # Vérifiez le nombre de handlers pour éviter les doublons
