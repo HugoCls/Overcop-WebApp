7ea5c99612c0d9f8ff40cac79e3b2e59c87e1c53
HugoCls <hugocls.pro@gmail.com>
2023-11-24 20:33:32 +0100

    Update

--- pages/Test.py
+++ pages/Test.py
@@ -27,7 +27,7 @@ if st.button('Folders & Files'):
             st.text(os.path.join(dossier_parent, fichier))
 
 
-#log.basicConfig(filename=log_file_path, level=log.INFO)#, format='%(asctime)s [%(levelname)s] %(filename)s - %(message)s')
+log.basicConfig(filename=log_file_path, level=log.INFO, format='%(asctime)s [%(levelname)s] %(filename)s - %(message)s')
 """
 if len(log.getLogger('').handlers) < 2:  # Vérifiez le nombre de handlers pour éviter les doublons
     temp_file_handler = log.FileHandler('data/temp_logs.log')
