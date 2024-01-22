4d2c40bf3300d4e6edc9324a5a84b50d7f890ec5
HugoCls <hugocls.pro@gmail.com>
2024-01-22 15:39:41 +0100

    Update

--- pages/Exceptions.py
+++ pages/Exceptions.py
@@ -20,41 +20,33 @@ df_Database = conn.query('SELECT * FROM `ov_Database`')
 
 Database_Names = list(df_Database['Name'].unique())
 
-col1, col2 = st.columns([4,1])
+pairs_to_add = st.multiselect(
+    'Select pairs to add as an exception',
+    Database_Names)
 
-with col1:
-    pairs_to_add = st.multiselect(
-        'Select pairs to add as an exception',
-        Database_Names)
+if st.button('Add'):
+    with conn.session as s:
+        for Name in pairs_to_add:
+            try:
+                sql_expression = text(f'INSERT INTO scraping_exceptions (Name) VALUES (:name)')
+                s.execute(sql_expression, {'name': Name})
+                st.text(f"Added **'{Name}'**")
+            except:
+                st.text(f"*'{Name}'* was already in exceptions")
 
-with col2:
-    if st.button('Add'):
-        with conn.session as s:
-            for Name in pairs_to_add:
-                try:
-                    sql_expression = text(f'INSERT INTO scraping_exceptions (Name) VALUES (:name)')
-                    s.execute(sql_expression, {'name': Name})
-                    st.text(f"Added **'{Name}'**")
-                except:
-                    st.text(f"*'{Name}'* was already in exceptions")
+        s.commit()
 
-            s.commit()
+pairs_to_delete = st.multiselect(
+    'Select pairs delete from exceptions table',
+    Exceptions_Names)
 
-col1_, col2_ = st.columns([4,1])
+if st.button('Delete'):
+    with conn.session as s:
+        for Name in pairs_to_delete:
+            try:
+                s.execute('DELETE FROM scraping_exceptions WHERE Name = :name;', {'name': Name})
+                st.text(f"Deleted *'{Name}'*")
+            except:
+                st.text(f"*'{Name}'* was not found in exceptions")
 
-with col1_:
-    pairs_to_delete = st.multiselect(
-        'Select pairs delete from exceptions table',
-        Exceptions_Names)
-
-with col2_:
-    if st.button('Delete'):
-        with conn.session as s:
-            for Name in pairs_to_delete:
-                try:
-                    s.execute('DELETE FROM scraping_exceptions WHERE Name = :name;', {'name': Name})
-                    st.text(f"Deleted *'{Name}'*")
-                except:
-                    st.text(f"*'{Name}'* was not found in exceptions")
-
-            s.commit()
+        s.commit()
