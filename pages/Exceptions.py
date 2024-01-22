5fd3356347926b47041a02c648138bd769089a12
HugoCls <hugocls.pro@gmail.com>
2024-01-22 14:32:17 +0100

    Add sql tables for exceptions

--- pages/Exceptions.py
+++ pages/Exceptions.py
@@ -1,16 +1,44 @@
 import streamlit as st
 import pandas as pd
 
-from sqlalchemy import create_engine
-
-engine = create_engine(f'mysql://{st.secrets["MYSQL_USERNAME"]}:{st.secrets["MYSQL_PASSWORD"]}@{st.secrets["VPS_IP"]}/overcop')
+conn = st.experimental_connection('overcop', type='sql')
 
 st.title("Scraping Exceptions")
 
-query = """
-SELECT * FROM scraping_exceptions
-"""
+df_scraping_exceptions = conn.query('SELECT * FROM scraping_exceptions')
 
-df_scraping_exceptions = pd.read_sql(query, con=engine)
+Exceptions_Names = list(df_scraping_exceptions['Name'].unique())
 
 st.dataframe(df_scraping_exceptions, hide_index=True)
+
+df_Database = conn.query('SELECT * FROM Database')
+
+Database_Names = list(df_Database['Title'].unique())
+
+col1, col2 = st.columns([4,1])
+
+with col1:
+    pairs_to_add = st.multiselect(
+        'Select pairs to add as an exception',
+        Database_Names)
+
+with col2:
+    if st.button('Add'):
+        with conn.session as s:
+            for Name in pairs_to_add:
+                s.execute('INSERT INTO scraping_exceptions (Name) VALUES (?);', (Name,))
+
+        s.commit()
+
+with col1:
+    pairs_to_delete = st.multiselect(
+        'Select pairs delete from exceptions table',
+        Exceptions_Names)
+
+with col2:
+    if st.button('Delete'):
+        with conn.session as s:
+            for Name in pairs_to_delete:
+                s.execute('DELETE FROM scraping_exceptions WHERE Name = ?;', (Name,))
+        
+        s.commit()
